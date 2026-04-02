5a375241ebfeaa85b3feec054a4edae8fec17925
apm96 <andreu.alumne96@gmail.com>
2026-04-02 19:34:08 +0200

    feat: Afegides esl html de login i signup, també els dominis de account, falta enllçar-ho amb db

--- Strike1v1/urls.py
+++ Strike1v1/urls.py
@@ -15,8 +15,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
-
+from django.urls import include, path
+from web.views import SignUpView
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('accounts/signup/', SignUpView.as_view(), name='signup'),
+    path('accounts/', include('django.contrib.auth.urls')),
+    path('', include('blog.urls')),
 ]
