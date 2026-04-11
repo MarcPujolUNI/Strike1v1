6123eb7a12eb555c6bd968f81989387ba5b97f0b
apm96 <andreu.alumne96@gmail.com>
2026-04-11 10:42:17 +0200

    feat: afegida base fins a step 2 del projecte de classe

--- web/urls.py
+++ web/urls.py
@@ -14,10 +14,10 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
-from django.urls import include, path
+from django.urls import path
 from web import views
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('web.urls')),
+    path('', views.home, name='home'),
+    path('accounts/signup/', SignUpView.as_view(), name='signup'),
+    path('accounts/', include('django.contrib.auth.urls')),
 ]
