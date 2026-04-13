2d57a05bb4a229d586bdc8787319fb0ff0efc066
Marc Pujol Navajo <mpn9@alumnes.udl.cat>
2026-04-14 00:22:36 +0200

    feat: added base.html, separated with components header.html and footer.html and added a basic landing page. Also added to static folder the css and js with some basic configuration (suggested to change in the nearly future) and modified slightly the urls.py and views.py from web folder to test the .html's and modified global urls.py to add web folder urls.py.

---
+++ static/js/tailwind-config.js
@@ -0,0 +1,25 @@
+tailwind.config = {
+    theme: {
+        extend: {
+            colors: {
+                "bg-dark": "#1a1a1b",
+                "card-dark": "#2e2e2e",
+                "brand-red": "#cc3333",
+                "brand-red-hover": "#e04343",
+                "title-blue-light": "#0058e6",
+                "title-blue-dark": "#003db3",
+                "border-black": "#000000",
+                "btn-blue-start": "#4b91d3",
+                "btn-blue-end": "#1e4d80",
+                "btn-green-start": "#7fb33d",
+                "btn-green-end": "#4e6d26",
+                "header-gray-start": "#555555",
+                "header-gray-end": "#111111",
+                "footer-link": "#a0a0a0",
+            },
+            fontFamily: {
+                "tahoma": ["Tahoma", "Verdana", "sans-serif"],
+            }
+        }
+    }
+}
