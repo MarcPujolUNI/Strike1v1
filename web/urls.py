592375e1f512ecc1f4c3c85d315bd905787b62e4
Marc Pujol Navajo <mpn9@alumnes.udl.cat>
2026-04-19 01:40:40 +0200

    feat: unified the window container visual to be consistent between all the pages. Also added a little shadow to the header and changed button message from 500 error web page.

--- web/urls.py
+++ web/urls.py
@@ -9,7 +9,7 @@ urlpatterns = [
     path('login', MyLoginView.as_view(), name='login'),
     path('leaderboard', views.leaderboard, name='leaderboard'),
     path('play', views.play, name='play'),
-    path('tos/', views.terms_of_service, name='tos'),
-    path('privacy/', views.privacy_policy, name='privacy'),
-    path('cookies/', views.cookie_policy, name='cookies'),
+    path('tos', views.terms_of_service, name='tos'),
+    path('privacy', views.privacy_policy, name='privacy'),
+    path('cookies', views.cookie_policy, name='cookies'),
 ]
