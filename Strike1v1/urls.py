e32f1a6bfebfd7bfcb548a043617cc2e65dde27f
Marc Pujol Navajo <mpn9@alumnes.udl.cat>
2026-04-19 23:22:29 +0200

    fix: added and issue that static images from /media folder didn't load when DEBUG environment variable was at False

--- Strike1v1/urls.py
+++ Strike1v1/urls.py
@@ -15,8 +15,9 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import include, path
+from django.urls import include, path, re_path
 from django.contrib.auth.views import LoginView
+from django.views.static import serve
 from web.views import SignUpView
 from web.forms import UserLoginForm
 from django.conf import settings
@@ -32,3 +33,7 @@ urlpatterns = [
 
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+else:
+    urlpatterns += [
+        re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
+    ]
