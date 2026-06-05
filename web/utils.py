7aa34624d30aa21a0cd9c13e908a4f94c4a3238e
PolO <polortega190000@gmail.com>
2026-06-05 18:53:42 +0200

    feat: added reviews HTML basic annotations

--- web/utils.py
+++ web/utils.py
@@ -2,7 +2,7 @@ from datetime import timedelta
 from django.utils import timezone
 from web.models import Match
 
-LOGS_DAYS = 90
+LOGS_DAYS = 30
 def clean_old_match_logs():
     cutoff = timezone.now() - timedelta(days=LOGS_DAYS)
     matches = Match.objects.filter(date__lt=cutoff, log_file__isnull=False)
